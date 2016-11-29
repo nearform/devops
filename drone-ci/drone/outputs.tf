6d75f5fe07fbcefc64e382e567ea2f9102a8e824
David Gonzalez <nodearth@gmail.com>
2016-11-29 11:52:30 +0000

    Added public/private IP flag

--- drone-ci/drone/outputs.tf
+++ drone-ci/drone/outputs.tf
@@ -7,8 +7,5 @@ output "drone-ip" {
 }
 
 output "reprovision-command" {
-  value = <<EOF
-    echo "[drone]\n${aws_instance.drone.public_ip}" > ${var.ansible_inventory_path};
-    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${var.ansible_inventory_path} ${path.module}/ansible/play.yml
-  EOF
+  value = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${var.ansible_inventory_path} --private-key ${var.private_key_path} ${path.module}/ansible/play.yml"
 }
