92feb95cae19cfe3e0b2eebb81dac74c65f12cb9
David Gonzalez <nodearth@gmail.com>
2016-11-29 11:59:17 +0000

    removed comments from the example file

--- drone-ci/terraform.tf
+++ drone-ci/terraform.tf
@@ -1,8 +1,7 @@
 # This file is only an example. Please see the README.md for more info
 
 module "drone" {
-  source = "./drone"
-  #source = "github.com/nearform/labs-devops//drone-ci/drone"
+  source = "github.com/nearform/labs-devops//drone-ci/drone"
   public_ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/eADYZQ1gUrxP4sfHi/H07dm9M0KnjYnmcY1Ek8rrPzR1gCEsC+JThZC446AdKHbsNHOIlo+XL5yNYwHKRwKgtnE0uGQi/yJNQvxQpE1fqp/cCRQxoJZ34DJkO0HJAtq4miU/dMLTsmLSDR6VOB10SDF7kwMxpSveOrBBMe0dj/MgtlnQSJJBSpb/rfwCq0EWTmajcgx21F8/msBak/isPPYSi6IlKMwgSTbV4xjDsTcjww0BpyiWoUCw2CE9fDeZw5PdHqWXo895ENVtcHf9FdM8JoZks8mHLEnu5B813Ez+nWS9eJjwWmZq5LmIyVHJCrEohUcS8hX/qWErEfDX dgonzalez@Davids-iMac.local"
   private_key_path = "~/.ssh/kube_aws_rsa"
   provision_through_private_ip = false
