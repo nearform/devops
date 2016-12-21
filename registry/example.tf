08c9ca17ceac8e0ae13c911d97231d11a4a9e966
David Gonzalez <nodearth@gmail.com>
2016-12-21 13:29:45 +0000

    code review feedback

--- registry/example.tf
+++ registry/example.tf
@@ -1,9 +1,9 @@
 # This file is only an example. Please see the README.md for more info
 
 module "registry" {
-  source = "./registry"
-  keypair_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/eADYZQ1gUrxP4sfHi/H07dm9M0KnjYnmcY1Ek8rrPzR1gCEsC+JThZC446AdKHbsNHOIlo+XL5yNYwHKRwKgtnE0uGQi/yJNQvxQpE1fqp/cCRQxoJZ34DJkO0HJAtq4miU/dMLTsmLSDR6VOB10SDF7kwMxpSveOrBBMe0dj/MgtlnQSJJBSpb/rfwCq0EWTmajcgx21F8/msBak/isPPYSi6IlKMwgSTbV4xjDsTcjww0BpyiWoUCw2CE9fDeZw5PdHqWXo895ENVtcHf9FdM8JoZks8mHLEnu5B813Ez+nWS9eJjwWmZq5LmIyVHJCrEohUcS8hX/qWErEfDX dgonzalez@Davids-iMac.local"
-  private_key_path = "~/.ssh/kube_aws_rsa"
+  source = "https://github.com/nearform/labs-devops//registry"
+  keypair_key = "<your-public-key>"
+  private_key_path = "~/.ssh/<your-private-key-file>"
   registry_subnet_id = ""
   registry_vpc_id = ""
 }
